2590bf47dbb0a0300feff69225b7610086737c4b
Random-user420 <leon_schmid@tutanota.com>
2024-09-22 15:03:55 +0200

    feat(autologin): Created inpput mask and currently stored unencryped in local brwoser storage

--- popup.js
+++ popup.js
@@ -4,3 +4,11 @@ document.getElementById('changeColorBtn').addEventListener('click', () => {
         chrome.tabs.sendMessage(tabs[0].id, { color: color });
     });
 });
+
+document.getElementById('autologinBtn').addEventListener('click', () => {
+    const username = document.getElementById('autologinUsername').value;
+    const password = document.getElementById('autologinPassword').value;
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { pawssword: password, username: username });
+    });
+});
