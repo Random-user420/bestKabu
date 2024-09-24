af1204ee8708586e4cb13ac96cddbafd276f2f89
Leo G. <83966683+vLeov@users.noreply.github.com>
2024-09-24 17:47:20 +0200

    feat(toggleDarkMode): Made a better style for the whitemode.

--- toggleDarkMode.js
+++ toggleDarkMode.js
@@ -30,37 +30,45 @@ function setDarkModeState(state) {
     let hourCanceled;
     let hourChanged;
     let white;
+    let glyphicon;
+    let activeTab;
     let timerBackgound;
     if (state === true) {
         navColor = "#1f2124";
         backgroundColor = "#292b30";
         planColor = "#37393f";
-        lessonColor = "#434549!important";
+        lessonColor = "#434549";
         hourOverlay = "#67696d";
         hourCanceled = "#FF4041";
         hourChanged = "#fab433";
         white = "#DDDDDD";
+        activeTab = "#ffffff";
+        glyphicon = "#ffffff";
         timerBackgound = "#434549";
     }
     else {
         navColor = "#767174";
-        backgroundColor = "#f0f0f0";
-        planColor = "#97999f";
-        lessonColor = "#a3a5a9!important";
-        hourOverlay = "#97999d";
+        backgroundColor = "#ffffff";
+        planColor = "#bfbfbf";
+        lessonColor = "#a3a5a9";
+        hourOverlay = "#6e6e6e";
         hourCanceled = "#FF4041";
         hourChanged = "#fab433";
-        white = "#DDDDDD";
-        timerBackgound = "#939599";
+        white = "#000000";
+        activeTab = "#000000";
+        glyphicon = "#ffffff";
+        timerBackgound = "#A3A5A9";
     }
     document.documentElement.style.setProperty('--navColor', navColor);
     document.documentElement.style.setProperty('--backgroundColor', backgroundColor);
     document.documentElement.style.setProperty('--planColor', planColor);
+    document.documentElement.style.setProperty('--glyphicon', glyphicon);
     document.documentElement.style.setProperty('--lessonColor', lessonColor);
     document.documentElement.style.setProperty('--hourOverlay', hourOverlay);
     document.documentElement.style.setProperty('--hourCanceled', hourCanceled);
     document.documentElement.style.setProperty('--hourCanceled', hourCanceled);
     document.documentElement.style.setProperty('--hourChanged', hourChanged);
     document.documentElement.style.setProperty('--white', white);
+    document.documentElement.style.setProperty('--activeTab', activeTab);
     document.documentElement.style.setProperty('--timerBackgound', timerBackgound);
 }
