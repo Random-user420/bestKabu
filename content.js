12bd17d2456e86d4f95f3a3626055836d0ffdf73
Random-user420 <leon_schmid@tutanota.com>
2024-09-24 14:58:07 +0200

    refactor: changed messages, added comment for sending messages

--- content.js
+++ content.js
@@ -1,19 +1,19 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.color) {
+    if (request.changeColorBtn) {
         set_stored_color(request.color);
         const elements = document.querySelectorAll('.weekdayToday');
         elements.forEach((el) => {
             el.style.setProperty('fill', request.color, 'important');
         });
     }
-    else if (request.delLogin) {
+    else if (request.autologinresetBtn) {
         delete_credentials();
     }
-    else if (request.getEnableState) {
+    else if (request.get_AutologinState) {
         const autologinEnableState = getAutoLoginState();
         sendResponse({ autologinEnableState });
     }
-    else if (request.username || "", request.pawssword || "", request.enabled || true) {
+    else if (request.autologinBtn) {
         processCredentials(request.username, request.pawssword, request.enabled);
     }
 });
--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
+// chrome.tabs..... { <identifier>: true, parameter… });
 document.getElementById('changeColorBtn').addEventListener('click', () => {
     const color = document.getElementById('colorPicker').value;
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { color: color });
+        chrome.tabs.sendMessage(tabs[0].id, { changeColorBtn: true, color: color });
     });
 });
 
@@ -10,19 +11,19 @@ document.getElementById('autologinBtn').addEventListener('click', () => {
     const password = document.getElementById('autologinPassword').value;
     const enabled = document.getElementById('autologinCheckbox').checked;
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { pawssword: password, username: username, enabled: enabled });
+        chrome.tabs.sendMessage(tabs[0].id, { autologinBtn: true, pawssword: password, username: username, enabled: enabled });
     });
 });
 
 document.getElementById('autologinresetBtn').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { delLogin: true });
+        chrome.tabs.sendMessage(tabs[0].id, { autologinresetBtn: true });
     });
 });
 
 function get_AutologinState() {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { getEnableState: true }, (response) => {
+        chrome.tabs.sendMessage(tabs[0].id, { get_AutologinState: true }, (response) => {
             document.getElementById("autologinCheckbox").checked = response.autologinEnableState;
         });
     });
