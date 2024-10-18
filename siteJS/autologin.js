08f9b49c2d06ad9114c4450894c753839c667827
les <leon.schmid@ivfp.de>
2024-10-18 15:26:02 +0200

    refactor/moved files to folders

---
+++ siteJS/autologin.js
@@ -0,0 +1,111 @@
+function encrypt(password, encryptionKey) {
+    if (!password || !encryptionKey) {
+        throw new Error('Password and encryption key cannot be null or undefined');
+    }
+    let encryptedPassword = '';
+    for (let i = 0; i < password.length; i++) {
+        const passwordCharCode = password.charCodeAt(i);
+        const keyCharCode = encryptionKey.charCodeAt(i % encryptionKey.length);
+        const encryptedCharCode = passwordCharCode ^ keyCharCode;
+        if (isNaN(encryptedCharCode)) {
+            throw new Error('Error during XOR operation');
+        }
+        encryptedPassword += String.fromCharCode(encryptedCharCode);
+    }
+    return encryptedPassword;
+}
+
+
+function processCredentials(username, password, enabled, encryptEn, key) {
+    //Check if input is valid → if only pw or us or (XOR(key, encEn)) with different cominations
+    if (!(!(((username === "" || password === "" || (key === "" && encryptEn)) && (username !== "" != password !== "")) || (key === "" && encryptEn) || (key !== "" && !encryptEn)) || (password === "" && username === "" && key === ""))) {
+        return "failedInputValidation";
+    }
+    if (encryptEn) {
+        let password_ = encrypt(password, key);
+        let username_ = encrypt(username, key);
+        set_stored_credentials(username_, password_, enabled, encryptEn);
+    }
+    else {
+        set_stored_credentials(username, password, enabled, encryptEn);
+    }
+}
+
+function set_stored_credentials(en_usr, en_pw, enabled, encrypt) {
+    if (en_usr !== null && en_usr !== undefined && en_usr !== "" && en_pw !== null && en_pw !== undefined && en_pw !== "") {
+        localStorage.setItem("username", en_usr);
+        localStorage.setItem("password", en_pw);
+        localStorage.setItem("encrypt", encrypt);
+    }
+    localStorage.setItem("enabled", enabled);
+}
+
+function get_stored_credentials() {
+    const username = localStorage.getItem("username");
+    const password = localStorage.getItem("password");
+    if (username !== null && password !== null) {
+        return [username, password];
+    }
+    else {
+        
+        return [null, null];
+    }
+}
+
+function get_credentials_enabled() {
+    return localStorage.getItem("enabled");
+}
+
+function getEnrcyptEnabled() {
+    let enc = localStorage.getItem("encrypt");
+    if (enc == undefined) {
+        enc = false;
+    }
+    return enc;
+}
+
+function loginEncrypted(key) {
+    [username_, password_] = get_stored_credentials();
+    if (username_ == null || password_ == null) {
+        return;
+    } else if(key == null || key == undefined || key == "") {
+        return "failedInputValidation";
+    }
+    document.getElementById("UserName").value = encrypt(username_, key);
+    document.getElementById("Password").value = encrypt(password_, key);
+    sleep(300);
+    document.querySelector(".btn.btn-primary").click();
+    sleep(1000);
+}
+
+
+function delete_credentials() {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+    localStorage.setItem("enabled", false);
+    localStorage.setItem("encrypt", false);
+}
+
+function login() {
+    let [username_, password_] = get_stored_credentials();
+    if (username_ == null || password_ == null) {
+        return;
+    }
+    document.getElementById("UserName").value = username_;
+    document.getElementById("Password").value = password_;
+    sleep(300);
+    document.querySelector(".btn.btn-primary").click();
+    sleep(1000);
+}
+
+function getAutoLoginState() {
+    const autologinEnableState = localStorage.getItem("enabled");
+    if (autologinEnableState != null || autologinEnableState != undefined) {
+        if (autologinEnableState === "true") {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
+}
---
+++ siteJS/timer.js
@@ -0,0 +1,51 @@
+function showTimer() {
+    if (window.location.pathname.includes("Stundenplan")) {
+        let box = document.getElementById("stdplanheading");
+        if (box === null) {
+            box = document.querySelector('div[style="margin-left:10px;"]');
+        }
+
+        const timerElement = document.createElement("span");
+        timerElement.id = "timerText";
+
+        box.append(timerElement);
+
+        function updateTimerDisplay(minutes, seconds) {
+            let timerText;
+            if (isNaN(minutes)) {
+                timerText = "Schule zu Ende :)";
+            } else {
+                timerText = `${minutes}m ${seconds}s`;
+            }
+            document.querySelector("#timerText").textContent = timerText;
+        }
+
+        function calculateTimeDiff() {
+            const currentTime = new Date();
+
+            let nextLessonStart;
+            if (getTimeObject(timeTable[0].start[0], timeTable[0].start[1]) > currentTime) {
+                nextLessonStart = getTimeObject(timeTable[0].start[0], timeTable[0].start[1]);
+            }
+            else {
+                for (let i = 0; i < timeTable.length; i++) {
+                    const endTime = getTimeObject(timeTable[i].end[0], timeTable[i].end[1]);
+                    if (currentTime < endTime) {
+                        nextLessonStart = endTime;
+                        break;
+                    }
+                }
+            }
+
+            const timeDiff = Math.max(nextLessonStart - currentTime, 0);
+            const minutes = Math.floor(timeDiff / 60000);
+            const seconds = Math.floor((timeDiff % 60000) / 1000);
+
+            updateTimerDisplay(minutes, seconds);
+        }
+
+        calculateTimeDiff();
+
+        setInterval(calculateTimeDiff, 1000);
+    }
+}
