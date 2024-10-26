ee22ab1b883396d000b842f632b2c39aec26025b
Random-user420 <leon_schmid@tutanota.com>
2024-10-26 14:51:48 +0200

    init popupCom.js

---
+++ logic/popupCom.js
@@ -0,0 +1,36 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    switch(request.key) {
+        case "getPopupInitState":
+            sendResponse(getPopupInitState());
+            break;
+        case "colorButtonEvent":
+            response = setColor(request.values);
+            sendResponse({ failedInputValidation: response });
+            break;
+        case "loginCheckboxEvent":
+            setLoginState(request.values);
+            break;
+        case "loginEncCheckboxEvent":
+            setEncLoginState(request.values);
+            break;
+        case "loginSaveButtonEvent":
+            response = saveLogin(request.values);
+            sendResponse({ failedInputValidation: response });
+            break;
+        case "loginDeleteButtonEvent":
+            deleteLogin();
+            break;
+        case "loginEncButtonEvent":
+            onEncLogin();
+            break;
+    }
+});
+
+function getPopupInitState() {
+    return {
+        popupState: isPopupState(),
+        darkmodeState: isDarkModeState(),
+        loginState: isLoginState(),
+        encState: isEncState()
+    }
+}
